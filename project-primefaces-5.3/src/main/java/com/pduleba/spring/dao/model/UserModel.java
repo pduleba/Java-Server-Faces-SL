5536e798ec2b961d8a03ef798575f86cd59f1512
pduleba <pduleba@pgs-soft.com>
2015-11-24 14:53:05 +0100

    Fixes

--- project-primefaces-5.3/src/main/java/com/pduleba/spring/dao/model/UserModel.java
+++ project-primefaces-5.3/src/main/java/com/pduleba/spring/dao/model/UserModel.java
@@ -2,19 +2,22 @@ package com.pduleba.spring.dao.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="users", schema="pduleba")
-public @Data class UserModel {
+@SequenceGenerator(name="user_db_seq", allocationSize=1, sequenceName="USERS_SEQ")
+public @Data @NoArgsConstructor class UserModel {
 
 	@Id
-	@SequenceGenerator(name="USERS_SEQ", allocationSize=1)
-	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="user_db_seq")
 	private Long id;
 	
 	@Column(name="name")
