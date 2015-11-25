5d7ab1a4bb916799f46ded2fa4e18f91d32e14e8
pduleba <pduleba@pgs-soft.com>
2015-11-25 13:17:26 +0100

    Session Factory Bean reconfiguration

--- project-primefaces-5.3/src/main/java/com/pduleba/spring/dao/UserDaoImpl.java
+++ project-primefaces-5.3/src/main/java/com/pduleba/spring/dao/UserDaoImpl.java
@@ -3,8 +3,6 @@ package com.pduleba.spring.dao;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
@@ -15,8 +13,6 @@ import com.pduleba.spring.dao.model.UserModel;
 @Repository
 @Transactional
 public class UserDaoImpl extends HibernateDaoSupport implements UserDao {
-
-	private static final Logger LOG = LoggerFactory.getLogger(UserDaoImpl.class);
 	
 	// DI by Constructor (you can use init method/setter)
 	@Autowired
